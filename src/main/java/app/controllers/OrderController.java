8ee00ef7a550ff56e1728f5598489ff840eb4c79
AllanChandler <cph-ac326@cphbusiness.dk>
2024-05-12 10:22:46 +0200

    Update OrderController.java

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -2,6 +2,8 @@ package app.controllers;
 
 import app.entities.Order;
 import app.entities.OrderLine;
+import app.entities.User;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.OrderLineMapper;
 import app.persistence.OrderMapper;
@@ -15,6 +17,7 @@ public class OrderController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         // Definer ruten til at vise ordrelisten
         app.get("/orderList", ctx -> showOrderList(ctx, connectionPool));
+        app.post("/Inquiry", ctx -> createOrder(ctx, connectionPool));
     }
 
     public static void showOrderList(Context ctx, ConnectionPool connectionPool) {
@@ -32,6 +35,31 @@ public class OrderController {
         } catch (Exception e) {
             // Håndter eventuelle undtagelser
             ctx.status(500).result("Fejl ved hentning af ordreliste eller linjer: " + e.getMessage());
+        }
+    }
+
+    private static void createOrder(Context ctx, ConnectionPool connectionPool) {
+        User currentUser = ctx.sessionAttribute("currentUser");
+
+        if (currentUser == null) {
+            ctx.attribute("message", "Du skal logge på for at oprette en bestilling.");
+            ctx.render("designCarport.html");
+            return;
+        }
+
+        int user_id = currentUser.getUserId();
+        int carport_length = Integer.parseInt(ctx.formParam("carport_length"));
+        int carport_width = Integer.parseInt(ctx.formParam("carport_width"));
+        int toolroom_length = Integer.parseInt(ctx.formParam("toolroom_length"));
+        int toolroom_width = Integer.parseInt(ctx.formParam("toolroom_width"));
+
+
+        try {
+            OrderMapper.createOrder(user_id, carport_length, carport_width, toolroom_length, toolroom_width, connectionPool);
+            ctx.render("orderConfirmation.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("message", "Noget gik galt. prøv igen");
+            ctx.render("designCarport.html");
         }
     }
 
