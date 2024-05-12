1f0ad6462f59cd0cf36d961442bdfc3142da3295
AllanChandler <cph-ac326@cphbusiness.dk>
2024-05-12 10:22:55 +0200

    Update PartList.java

--- src/main/java/app/entities/PartList.java
+++ src/main/java/app/entities/PartList.java
@@ -1,2 +1,70 @@
-package app.entities;public class PartList {
+package app.entities;
+
+public class PartList {
+
+    private String materialDescription;
+    private int length;
+    private int quantity;
+    private String unit;
+    private String orderLineDescription;
+
+    public PartList(String materialDescription, int length, int quantity, String unit, String orderLineDescription) {
+        this.materialDescription = materialDescription;
+        this.length = length;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.orderLineDescription = orderLineDescription;
+    }
+
+    public String getMaterialDescription() {
+        return materialDescription;
+    }
+
+    public void setMaterialDescription(String materialDescription) {
+        this.materialDescription = materialDescription;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getOrderLineDescription() {
+        return orderLineDescription;
+    }
+
+    public void setOrderLineDescription(String orderLineDescription) {
+        this.orderLineDescription = orderLineDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "PartlistItem{" +
+                "materialDescription='" + materialDescription + '\'' +
+                ", length=" + length +
+                ", quantity=" + quantity +
+                ", unit='" + unit + '\'' +
+                ", orderLineDescription='" + orderLineDescription + '\'' +
+                '}';
+    }
+
 }
