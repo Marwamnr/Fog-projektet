d1802cbaeae5c9307f4d81a749448387242f6f25
AllanChandler <cph-ac326@cphbusiness.dk>
2024-05-13 13:32:42 +0200

    Create CalculatorPost.java

---
+++ src/test/java/app/services/CalculatorPost.java
@@ -0,0 +1,31 @@
+package app.services;
+
+import app.persistence.ConnectionPool;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorPost {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fog";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    @BeforeAll
+    static void setup()
+    {
+
+    }
+
+
+    @Test
+    void calcPostQuantity()
+    {
+        Calculator calculator = new Calculator(300, 599, connectionPool);
+        assertEquals(8, calculator.calcPostsQuantity());
+    }
+}
