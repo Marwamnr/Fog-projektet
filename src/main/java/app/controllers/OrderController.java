efc6aaefa6a0b95d8b60f6d25dcc87b6700e1093
AllanChandler <cph-ac326@cphbusiness.dk>
2024-05-14 14:06:42 +0200

    Update OrderController.java

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -18,7 +18,7 @@ public class OrderController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/orderList", ctx -> showOrderList(ctx, connectionPool));
 
-        app.post("/Inquiry", ctx -> sendRequest(ctx, connectionPool));
+        app.post("/Inquiry", ctx -> createOrder(ctx, connectionPool));
 
     }
 
@@ -41,7 +41,7 @@ public class OrderController {
     }
 
 
-    private static void sendRequest(Context ctx, ConnectionPool connectionPool) {
+    private static void createOrder(Context ctx, ConnectionPool connectionPool) {
 
         User currentUser = ctx.sessionAttribute("currentUser");
         if (currentUser == null) {
@@ -51,7 +51,7 @@ public class OrderController {
         }
 
         int status = 1; // Hardcoded for now
-        int toolroomWidth = Integer.parseInt(ctx    .formParam("toolroom_width"));
+        int toolroomWidth = Integer.parseInt(ctx.formParam("toolroom_width"));
         int toolroomLength = Integer.parseInt(ctx.formParam("toolroom_length"));
         int carportWidth = Integer.parseInt(ctx.formParam("carport_width"));
         int carportLength = Integer.parseInt(ctx.formParam("carport_length"));
@@ -84,32 +84,6 @@ public class OrderController {
         }
     }
 
-
-
-    private static void createOrder(Context ctx, ConnectionPool connectionPool) {
-        User currentUser = ctx.sessionAttribute("currentUser");
-
-        if (currentUser == null) {
-            ctx.attribute("message", "Du skal logge på for at oprette en bestilling.");
-            ctx.render("designCarport.html");
-            return;
-        }
-
-        int user_id = currentUser.getUserId();
-        int carport_length = Integer.parseInt(ctx.formParam("carport_length"));
-        int carport_width = Integer.parseInt(ctx.formParam("carport_width"));
-        int toolroom_length = Integer.parseInt(ctx.formParam("toolroom_length"));
-        int toolroom_width = Integer.parseInt(ctx.formParam("toolroom_width"));
-
-
-        try {
-            OrderMapper.createOrder(user_id, carport_length, carport_width, toolroom_length, toolroom_width, connectionPool);
-            ctx.render("orderConfirmation.html");
-        } catch (DatabaseException e) {
-            ctx.attribute("message", "Noget gik galt. prøv igen");
-            ctx.render("designCarport.html");
-        }
-    }
 }
 
 
